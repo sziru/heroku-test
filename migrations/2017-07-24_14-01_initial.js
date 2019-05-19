8beba510f42ec1e1a9043edaf80ae4c18c81bfa6
Kyeonghun Min <sunnyziru@gmail.com>
2019-05-19 19:57:00 +0900

    repository init

---
+++ migrations/2017-07-24_14-01_initial.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("locations", (table) => {
+    table.increments().index();
+
+    table.float("latitude");
+
+    table.float("longitude");
+
+    table.text("name").notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('locations');
+};
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div>
+      <Map />
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Map from "./components/Map";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    Map,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
